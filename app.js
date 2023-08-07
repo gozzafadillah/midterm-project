c7e9aff9503f181a840c0092f5495e2f02fbac69
gozzafadillah <gozzafadillah@gmail.com>
2023-08-07 20:54:50 +0700

    added cors

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // Gunakan rute-rute yang telah didefinisikan
 const SetupVideoRoutes = require("./src/routes/video.route");
@@ -16,11 +21,11 @@ const SetupUserRoutes = require("./src/routes/user.route");
 const userRoutes = SetupUserRoutes();
 const SetupCommentRoutes = require("./src/routes/comment.route");
 const commentRoutes = SetupCommentRoutes();
-app.use("/videos", videoRoutes);
-app.use("/products", productRoutes);
-app.use("/users", userRoutes);
-app.use("/comments", commentRoutes);
-app.get("/", (req, res) => {
+app.use("/videos", cors(corsOptions), videoRoutes);
+app.use("/products", cors(corsOptions), productRoutes);
+app.use("/users", cors(corsOptions), userRoutes);
+app.use("/comments", cors(corsOptions), commentRoutes);
+app.get("/", cors(corsOptions), (req, res) => {
   res.send("Hello World!");
 });
 
